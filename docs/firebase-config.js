5e44beae009f49eda4a80aa749b78a7f1cfe1ff6
pavoniingrifati <pavoniingrifati@gmail.com>
2026-01-17 20:38:11 +0100

    Add Firebase configuration file

---
+++ docs/firebase-config.js
@@ -0,0 +1,12 @@
+// docs/firebase-config.js
+export const firebaseConfig = {
+  apiKey: "PASTE",
+  authDomain: "PASTE",
+  projectId: "PASTE",
+  storageBucket: "PASTE",
+  messagingSenderId: "PASTE",
+  appId: "PASTE",
+};
+
+// Se le tue Functions sono in un'altra region, cambia qui (es: "europe-west1")
+export const functionsRegion = "us-central1";
