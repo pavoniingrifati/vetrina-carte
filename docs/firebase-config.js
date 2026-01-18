94a14e539ecc126b82b939f12ae19522b5685ee4
pavoniingrifati <pavoniingrifati@gmail.com>
2026-01-18 08:31:29 +0100

    Update firebase-config.js

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -1,11 +1,11 @@
 // docs/firebase-config.js
 export const firebaseConfig = {
-  apiKey: "PASTE",
-  authDomain: "PASTE",
-  projectId: "PASTE",
-  storageBucket: "PASTE",
-  messagingSenderId: "PASTE",
-  appId: "PASTE",
+  apiKey: "AIzaSyDv23gasgBuAtPeDJeztyJ5P2S7NWq1svo",
+  authDomain: "fantaball-9e19c.firebaseapp.com",
+  projectId: "fantaball-9e19c",
+  storageBucket: "fantaball-9e19c.appspot.com",
+  messagingSenderId: "683304379837",
+  appId: "1:683304379837:web:d2bbe7a814e2e40e075ed4",
 };
 
 // Se le tue Functions sono in un'altra region, cambia qui (es: "europe-west1")
