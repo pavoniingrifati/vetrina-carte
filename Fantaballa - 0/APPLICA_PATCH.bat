534cafb150fd0ac351ff51bff005a4d51fb38cb6
pavoniingrifati <pavoniingrifati@gmail.com>
2026-08-04 07:34:57 +0200

    vai

--- Fantaballa - 0/APPLICA_PATCH.bat
+++ Fantaballa - 0/APPLICA_PATCH.bat
@@ -1,25 +1,10 @@
 @echo off
 setlocal
-cd /d "%~dp0"
-set "TARGET=%~1"
-if not defined TARGET (
-  echo Trascina la cartella principale di Fantaballa sopra questo file,
-  echo oppure esegui: APPLICA_PATCH.bat "C:\percorso\Fantaballa - 0"
-  echo.
-  pause
-  exit /b 1
-)
-if not exist "%TARGET%\direttore-sportivo.html" (
-  echo ERRORE: la cartella indicata non sembra la cartella principale di Fantaballa.
-  pause
-  exit /b 1
-)
-copy /Y "direttore-sportivo.html" "%TARGET%\direttore-sportivo.html" >nul
-if not exist "%TARGET%\assets\director" mkdir "%TARGET%\assets\director"
-copy /Y "assets\director\director.js" "%TARGET%\assets\director\director.js" >nul
-copy /Y "CORREZIONE_QUOTA_SALVATAGGIO.txt" "%TARGET%\CORREZIONE_QUOTA_SALVATAGGIO.txt" >nul
+set ROOT=%~dp0
+set TARGET=%cd%
+echo Copia manualmente questi file nella cartella del progetto:
 echo.
-echo Patch applicata correttamente.
-echo Il salvataggio esistente verra compattato automaticamente.
-echo Non serve aggiornare Google Apps Script.
+echo assets\director\director.js
+echo assets\director\director.css
+echo.
 pause
