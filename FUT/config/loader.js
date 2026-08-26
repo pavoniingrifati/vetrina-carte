3f8508978ee036b46358b61853fe8bab81a1383b
pavoniingrifati <pavoniingrifati@gmail.com>
2026-08-26 18:07:34 +0200

    fut

--- FUT/config/loader.js
+++ FUT/config/loader.js
@@ -2,5 +2,5 @@
   const params = new URLSearchParams(location.search);
   const mode = params.get('mode') === 'gotham' ? 'gotham' : 'fantaballa';
   window.FUTTU_MODE = mode;
-  document.write('<script src="config/' + mode + '.js?v=20260726-opening-base-premium-v1"></' + 'script>');
+  document.write('<script src="config/' + mode + '.js?v=20260826-worldcup-v2"></' + 'script>');
 })();
